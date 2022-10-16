bda4011866b068f1538e5529f0cf15c7f6396fa3
sashaterno <sashaterno2002@gmail.com>
2022-10-16 23:14:41 +0200

    added a house price estimator

--- javascript/javascript1/week1/script.js
+++ javascript/javascript1/week1/script.js
@@ -20,3 +20,37 @@ if (shouldShowResultInDogYears) {
 } else {
     console.log("Your dog will be " + humanYear + " human years old in " + dogYearFuture)
 };
+
+
+
+// Housey pricey (A house price estimator)
+
+let widthPeter = 8;
+let depthPeter = 10;
+let heightPeter = 10;
+let gardenSizePeter = 100; //in m2
+let houseCostPeter = 2500000;
+let volumeInMetersPeter = widthPeter * depthPeter * heightPeter;
+let houseRealPricePeter = volumeInMetersPeter * 2.5 * 1000 + gardenSizePeter * 300;
+console.log("Real price for Peter house is " + houseRealPricePeter);
+
+if (houseRealPricePeter < houseCostPeter) {
+    console.log("This house cost too much!")
+} else {
+    console.log("This house cost too little!")
+};
+
+let widthJulia = 5;
+let depthJulia = 11;
+let heightJulia = 8;
+let gardenSizeJulia = 70; //in m2
+let houseCostJulia = 1000000;
+let volumeInMetersJulia = widthJulia * depthJulia * heightJulia;
+let houseRealPriceJulia = volumeInMetersJulia * 2.5 * 1000 + gardenSizeJulia * 300;
+console.log("Real price for Julia house is " + houseRealPriceJulia);
+
+if (houseRealPriceJulia < houseCostJulia) {
+    console.log("This house cost too much!")
+} else {
+    console.log("This house cost too little!")
+};
