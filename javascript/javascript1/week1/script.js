d7bf480685077e09da32e9a25515cd6cb021d9e1
sashaterno <sashaterno2002@gmail.com>
2022-10-16 22:48:12 +0200

    added a dog age calculator

--- javascript/javascript1/week1/script.js
+++ javascript/javascript1/week1/script.js
@@ -5,4 +5,18 @@ let yearFuture = 2045;
 let age = yearFuture - YEAR_OF_BIRTH;
 console.log("You will be " + age + " years old in " + yearFuture);
 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+// Goodboy-Oldboy (A dog age calculator)
+
+const DOG_YEAR_OF_BIRTH = 2015;
+let dogYearFuture = 2027;
+let humanYear = dogYearFuture - DOG_YEAR_OF_BIRTH;
+let dogYear = humanYear * 7;
+let shouldShowResultInDogYears = true;
+
+if (shouldShowResultInDogYears) {
+    console.log("Your dog will be " + dogYear + " dog years old in " + dogYearFuture)
+} else {
+    console.log("Your dog will be " + humanYear + " human years old in " + dogYearFuture)
+};
