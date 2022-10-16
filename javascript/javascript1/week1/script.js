2d9eefd515dff1eb3615805c72a8f1f25e6a7d3b
sashaterno <sashaterno2002@gmail.com>
2022-10-16 22:07:34 +0200

    added a future age calculator

---
+++ javascript/javascript1/week1/script.js
@@ -0,0 +1,8 @@
+// Age-ify (A future age calculator)
+
+const YEAR_OF_BIRTH = 2002;
+let yearFuture = 2045;
+let age = yearFuture - YEAR_OF_BIRTH;
+console.log("You will be " + age + " years old in " + yearFuture);
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~
