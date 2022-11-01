a73dcbaad1bbd3a76e002154764b6bdf0f9aaa17
sashaterno <sashaterno2002@gmail.com>
2022-11-01 17:36:41 +0100

    added series duration of my life

--- javascript/javascript1/week3/scripts.js
+++ javascript/javascript1/week3/scripts.js
@@ -35,3 +35,47 @@ function getTravelTime(speed, distance) {
 const travelTime = getTravelTime(60, 425);
 
 console.log(travelTime);
+
+
+// Series duration of my life
+
+const seriesDurations = [
+    {
+        title: "Witcher",
+        days: 0,
+        hours: 16,
+        minutes: 0,
+    },
+    {
+        title: "Sherlock",
+        days: 0,
+        hours: 18,
+        minutes: 0,
+    },
+    {
+        title: "Vikings",
+        days: 2,
+        hours: 17,
+        minutes: 16,
+    },
+];
+
+function logOutSeriesText() {
+    const lifeSpan = 80 * 365 * 24 * 60;
+
+    let seriesTime = 0;
+
+    for (let element of seriesDurations) {
+        const seriesDuration = (element.days * 24 * 60) + (element.hours * 60) + element.minutes;
+        const percentOfLife = (100 * (seriesDuration / lifeSpan));
+        seriesTime += seriesDuration;
+
+        console.log(`${element.title} took ${percentOfLife.toFixed(3)}% of my life`);
+    }
+
+    const totalTime = (100 * (seriesTime / lifeSpan));
+
+    console.log(`In total that is ${totalTime.toFixed(2)}% of my life`);
+}
+
+logOutSeriesText();
